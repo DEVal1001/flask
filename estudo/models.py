4359437843337e480e53dabe26e91e752f0f24df
DEVal1001 <val.engsoft@gmail.com>
2025-04-17 12:19:30 -0300

    salvandi itens no banco 01

---
+++ estudo/models.py
@@ -0,0 +1,13 @@
+from estudo import db
+from datetime import datetime
+
+class Contato(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data_criacao = db.Column(db.DateTime, default=datetime.now)
+    nome = db.Column(db.String(50), nullable=True)
+    email = db.Column(db.String(120), nullable=True, unique=True)
+    assunto = db.Column(db.String(200), nullable=True)
+    mensagem = db.Column(db.Text, nullable=True)
+    respondido = db.Column(db.Integer, default=0)
+
+    
--- estudo/views.py
+++ estudo/views.py
@@ -1,6 +1,40 @@
-from estudo import app
-from flask import render_template
+from estudo import app, db
+from flask import render_template, url_for, request
+from estudo.models import Contato
 
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    usuario = 'Lucas'
+    idade = 22
+    context = {
+        'usuario': usuario,
+        'idade': idade,
+        
+    }
+    return render_template('index.html', context=context)
+
+@app.route('/contato/', methods=['GET', 'POST'])
+def novapagina():
+    context = {}
+        
+    if request.method == 'GET':
+        pesquisa = request.args.get('pesquisa')
+        print('GET:',pesquisa)
+        context.update({'pesquisa': pesquisa})
+        
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        email = request.form.get('email')
+        assunto = request.form.get('assunto')
+        mensagem = request.form.get('mensagem')
+        
+        contato = Contato(
+            nome=nome, 
+            email=email, 
+            assunto=assunto, 
+            mensagem=mensagem
+            )
+        db.session.add(contato)
+        db.session.commit()
+        
+    return render_template('contato.html', context=context)
